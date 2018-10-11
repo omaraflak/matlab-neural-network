84544ff0c8eea3a9586e21e09eed42ce5b4935fc
Omar Aflak <aflakomar@gmail.com>
2018-10-11 22:38:34 +0100

    added Activation class

---
+++ Activation.m
@@ -0,0 +1,26 @@
+classdef Activation < handle
+    properties(Access = private)
+        activation
+        derivative
+    end
+    
+    methods(Access = public)
+        function self = Activation(name)
+            if name=="tanh"
+                self.activation = @(x) tanh(x);
+                self.derivative = @(x) 1 - tanh(x)^2;
+            elseif name=="sigmoid"
+                self.activation = @(x) 1.0/(1.0+exp(-x));
+                self.derivative = @(x) exp(-x)/((1+exp(-x))^2);
+            end
+        end
+        
+        function activation = get_activation(self)
+            activation = self.activation;
+        end
+        
+        function activation_prime = get_activation_prime(self)
+            activation_prime = self.derivative;
+        end
+    end    
+end
