24bf307c0a40a3218f1542dde8f7deeedc2ed096
Omar Aflak <aflakomar@gmail.com>
2018-10-11 21:18:31 +0100

    cleaned

--- Network.m
+++ Network.m
@@ -10,9 +10,9 @@ classdef Network < handle
         
         function output = predict(self, input)            
             layers_count = size(self.layers, 2);
-            input_count = size(input, 1);
-            output = zeros(input_count, self.layers{layers_count}.get_output_size());
-            for j=1:input_count
+            samples_count= size(input, 1);
+            output = zeros(samples_count, self.layers{layers_count}.get_output_size());
+            for j=1:samples_count
                 out = input(j,:);
                 for i=1:layers_count
                     out = self.layers{1,i}.forward_propagation(out);
@@ -23,19 +23,17 @@ classdef Network < handle
         
         function [] = fit(self, input, output, epochs, learning_rate)
             layers_count = size(self.layers, 2);
-            input_count = size(input, 1);
-            err = 0;
+            samples_count = size(input, 1);
             for i=1:epochs
                 pred = self.predict(input);
-                for j=1:input_count
-                    error = (output(j,:) - pred(j,:)).^2;
-                    err = err + mean(error, 'all');
+                for j=1:samples_count
+                    derror = pred(j,:) - output(j,:);
                     for k=layers_count:-1:1
-                        error = self.layers{1,k}.back_propagation(error, learning_rate);
+                        derror = self.layers{1,k}.back_propagation(derror, learning_rate);
                     end
                 end
-                err = err / input_count;
-                fprintf('%d/%d   err=%f\n',i,epochs,err);
+                error = mean((output - pred).^2, 'all');
+                fprintf('%d/%d   err=%f\n', i, epochs, error);
             end
         end
     end
