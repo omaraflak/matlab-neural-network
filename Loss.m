ee9e084837a8f3e6b4a02df1a535c70e7e340abe
Omar Aflak <aflakomar@gmail.com>
2018-10-11 23:49:04 +0100

    added cross-entropy loss

--- Loss.m
+++ Loss.m
@@ -9,6 +9,9 @@ classdef Loss < handle
             if name=="mse"
                 self.loss = @(y_true, y_pred) 0.5*(y_true - y_pred).^2;
                 self.loss_derivative = @(y_true, y_pred) (y_pred - y_true);
+            elseif name=="cross-entropy"
+                self.loss = @(y_true, y_pred) -y_true*log10(y_pred);
+                self.loss_derivative = @(y_true, y_pred) -y_true/(y_pred*log(10));
             end
         end
         
