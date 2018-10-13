bed8cb373ae83a29f50a06c7eb1d36aceb45110b
Omar Aflak <aflakomar@gmail.com>
2018-10-13 12:03:38 +0100

    added digits reco + xor exampes

---
+++ examples/xor/xor.m
@@ -0,0 +1,21 @@
+function [] = xor()
+    % add path to neural network
+    addpath('../../sources');
+    
+    % create a 3-layer neural network
+    net = Network();
+    net.add_layer(FullyConnectedLayer(2, 3));
+    net.add_layer(ActivationLayer(3, Activation("tanh")));
+    net.add_layer(FullyConnectedLayer(3,1));
+    net.add_layer(ActivationLayer(1, Activation("tanh")));
+    
+    % IO data
+    input = [0 0 ; 0 1 ; 1 0 ; 1 1];
+    output = [0 ; 1 ; 1 ; 0];
+    
+    % start training on 1000 iterations, with a learning rate of 0.2
+    net.fit(input, output, Loss("mse"), 1000, 0.2);
+    
+    % test network
+    net.predict(input)
+end
