a877cf27ade04465abf8e847cae9a61a7e09ec70
Omar Aflak <aflakomar@gmail.com>
2018-10-14 21:21:22 +0100

    added FlattenLayer to CNN

--- examples/cnn/main.m
+++ examples/cnn/main.m
@@ -7,12 +7,12 @@ function [] = main()
     net.add_layer(MaxPoolLayer([6 6 1], [3 3 1]));
     net.add_layer(ConvolutionalLayer([4 4 1], [3 3 1], 1));
     net.add_layer(ActivationLayer([2 2 1], Activation('sigmoid')));
-    net.add_layer(MaxPoolLayer([2 2 1], [2 2 1]));
+    net.add_layer(FlattenLayer([2 2 1]));
     
     net.build(Loss('mse'), 0.2);
        
     input = rand(10,10);
-    output = rand(1,1);
+    output = rand(1,4);
     
     net.fit(input, output, 100);
     error = net.evaluate(input, output)
