288fd1057c2b7424b82e9055716bd9ee6ebe1529
Omar Aflak <aflakomar@gmail.com>
2018-10-14 18:14:53 +0100

    working example !

--- examples/cnn/main.m
+++ examples/cnn/main.m
@@ -2,14 +2,18 @@ function [] = main()
     addpath('../../sources');
     
     net = Network();
-    net.add_layer(ConvolutionalLayer([10 10], [3 3], 1));
+    net.add_layer(ConvolutionalLayer([10 10], [5 5], 1));
+    net.add_layer(ActivationLayer([6 6], Activation('sigmoid')));
+    net.add_layer(ConvolutionalLayer([6 6], [3 3], 1));
+    net.add_layer(ActivationLayer([4 4], Activation('sigmoid')));
     
     net.build(Loss('mse'), 0.1);
        
     input = rand(10,10);
-    output = rand(8,8);
+    output = rand(4,4);
     
     net.fit(input, output, 100);
-    
     net.predict(input)
+    output
+    error = net.evaluate(input, output)
 end
