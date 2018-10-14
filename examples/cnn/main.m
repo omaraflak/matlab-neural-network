d017f3f1029f9f91c257e6eb709c098cd22a01d6
Omar Aflak <aflakomar@gmail.com>
2018-10-14 23:11:43 +0100

    update cnn architecture

--- examples/cnn/main.m
+++ examples/cnn/main.m
@@ -4,12 +4,13 @@ function [] = main()
     fprintf('loading training data...');
     [x_train, y_train, x_test, y_test] = read_training('training');
     fprintf('done.\n');
-    
+        
     net = Network();
-    net.add_layer(ConvolutionalLayer([32 32], [3 3], 1));
-    net.add_layer(ActivationLayer([30 30 5], Activation('sigmoid')));
-    net.add_layer(FlattenLayer([30 30 5]));
-    net.add_layer(FullyConnectedLayer([1 4500], [1 10]));
+    net.add_layer(ConvolutionalLayer([32 32], [5 5], 3));
+    net.add_layer(ActivationLayer([28 28 3], Activation('sigmoid')));
+    net.add_layer(MaxPoolLayer([28 28 3], [9 9]));
+    net.add_layer(FlattenLayer([20 20 3]));
+    net.add_layer(FullyConnectedLayer([1 1200], [1 10]));
     net.add_layer(ActivationLayer([1 10], Activation('sigmoid')));
     
     net.build(Loss('mse'), 0.7);
