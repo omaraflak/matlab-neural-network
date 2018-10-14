af4b67c95e0688acda0237bf4ce7d88b28ff5697
Omar Aflak <aflakomar@gmail.com>
2018-10-14 20:53:06 +0100

    added maxpool after convolutional layers

--- examples/cnn/main.m
+++ examples/cnn/main.m
@@ -4,13 +4,15 @@ function [] = main()
     net = Network();
     net.add_layer(ConvolutionalLayer([10 10 1], [5 5 1], 1));
     net.add_layer(ActivationLayer([6 6 1], Activation('sigmoid')));
-    net.add_layer(ConvolutionalLayer([6 6 1], [3 3 1], 1));
-    net.add_layer(ActivationLayer([4 4 1], Activation('sigmoid')));
+    net.add_layer(MaxPoolLayer([6 6 1], [3 3 1]));
+    net.add_layer(ConvolutionalLayer([4 4 1], [3 3 1], 1));
+    net.add_layer(ActivationLayer([2 2 1], Activation('sigmoid')));
+    net.add_layer(MaxPoolLayer([2 2 1], [2 2 1]));
     
-    net.build(Loss('mse'), 0.1);
+    net.build(Loss('mse'), 0.2);
        
     input = rand(10,10);
-    output = rand(4,4);
+    output = rand(1,1);
     
     net.fit(input, output, 100);
     error = net.evaluate(input, output)
