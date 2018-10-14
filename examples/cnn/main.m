ea19417baed014f8b2ea3dd19862f0644ce27060
Omar Aflak <aflakomar@gmail.com>
2018-10-14 19:55:08 +0100

    cleaned

--- examples/cnn/main.m
+++ examples/cnn/main.m
@@ -2,15 +2,15 @@ function [] = main()
     addpath('../../sources');
     
     net = Network();
-    net.add_layer(ConvolutionalLayer([10 10 1], [5 5 1], 2));
-    net.add_layer(ActivationLayer([6 6 2], Activation('sigmoid')));
-    net.add_layer(ConvolutionalLayer([6 6 2], [3 3 2], 1));
-    net.add_layer(ActivationLayer([4 4 2], Activation('sigmoid')));
+    net.add_layer(ConvolutionalLayer([10 10 1], [5 5 1], 1));
+    net.add_layer(ActivationLayer([6 6 1], Activation('sigmoid')));
+    net.add_layer(ConvolutionalLayer([6 6 1], [3 3 1], 1));
+    net.add_layer(ActivationLayer([4 4 1], Activation('sigmoid')));
     
     net.build(Loss('mse'), 0.1);
        
     input = rand(10,10);
-    output = rand(4,4,2);
+    output = rand(4,4);
     
     net.fit(input, output, 100);
     error = net.evaluate(input, output)
