26b3560c78822a3db5d058c4a221a071bdedee87
Omar Aflak <aflakomar@gmail.com>
2018-10-14 19:51:06 +0100

    update example with depth

--- examples/cnn/main.m
+++ examples/cnn/main.m
@@ -2,18 +2,16 @@ function [] = main()
     addpath('../../sources');
     
     net = Network();
-    net.add_layer(ConvolutionalLayer([10 10], [5 5], 1));
-    net.add_layer(ActivationLayer([6 6], Activation('sigmoid')));
-    net.add_layer(ConvolutionalLayer([6 6], [3 3], 1));
-    net.add_layer(ActivationLayer([4 4], Activation('sigmoid')));
+    net.add_layer(ConvolutionalLayer([10 10 1], [5 5 1], 2));
+    net.add_layer(ActivationLayer([6 6 2], Activation('sigmoid')));
+    net.add_layer(ConvolutionalLayer([6 6 2], [3 3 2], 1));
+    net.add_layer(ActivationLayer([4 4 2], Activation('sigmoid')));
     
     net.build(Loss('mse'), 0.1);
        
     input = rand(10,10);
-    output = rand(4,4);
+    output = rand(4,4,2);
     
     net.fit(input, output, 100);
-    net.predict(input)
-    output
     error = net.evaluate(input, output)
 end
