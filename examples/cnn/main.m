851ac233149a135fb098df8e42455818f2727448
Omar Aflak <aflakomar@gmail.com>
2018-10-14 23:00:46 +0100

    applied shape changes into examples

--- examples/cnn/main.m
+++ examples/cnn/main.m
@@ -2,17 +2,17 @@ function [] = main()
     addpath('../../sources');
     
     net = Network();
-    net.add_layer(ConvolutionalLayer([10 10 1], [5 5 1], 1));
-    net.add_layer(ActivationLayer([6 6 1], Activation('sigmoid')));
-    net.add_layer(MaxPoolLayer([6 6 1], [3 3 1]));
-    net.add_layer(ConvolutionalLayer([4 4 1], [3 3 1], 1));
-    net.add_layer(ActivationLayer([2 2 1], Activation('sigmoid')));
-    net.add_layer(FlattenLayer([2 2 1]));
+    net.add_layer(ConvolutionalLayer([10 10], [5 5], 2));
+    net.add_layer(ActivationLayer([6 6 2], Activation('sigmoid')));
+    net.add_layer(MaxPoolLayer([6 6 2], [3 3]));
+    net.add_layer(ConvolutionalLayer([4 4 2], [3 3], 1));
+    net.add_layer(ActivationLayer([2 2 2], Activation('sigmoid')));
+    net.add_layer(FlattenLayer([2 2 2]));
     
-    net.build(Loss('mse'), 0.2);
+    net.build(Loss('mse'), 0.1);
        
     input = rand(10,10);
-    output = rand(1,4);
+    output = rand(1,8);
     
     net.fit(input, output, 100);
     error = net.evaluate(input, output)
