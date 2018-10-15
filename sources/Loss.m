04841270730a86927c45e508b4e517ac4d186fd7
Omar Aflak <aflakomar@gmail.com>
2018-10-15 23:50:38 +0100

    mae + neg_log

--- sources/Loss.m
+++ sources/Loss.m
@@ -12,10 +12,13 @@ classdef Loss < handle
             elseif name=="msle"
                 self.loss = @(y_true, y_pred) mean((log10(y_true+1)-log10(y_pred+1)).^2, 'all');
                 self.loss_derivative = @(y_true, y_pred) (-2/numel(y_true))*(log10(y_true+1)-log10(y_pred+1))/((log(10)*(y_pred+1)));
-            elseif name=="l2"
-                self.loss = @(y_true, y_pred) sum((y_true - y_pred).^2, 'all');
-                self.loss_derivative = @(y_true, y_pred) 2*(y_pred - y_true);
-            elseif name=="cross-entropy"
+            elseif name=="mae"
+                self.loss = @(y_true, y_pred) mean(abs(y_true - y_pred), 'all');
+                self.loss_derivative = @(y_true, y_pred) (y_pred - y_true)./(numel(y_true)*abs(y_true - y_pred));
+            elseif name=="neg_log_likelihood"
+                self.loss = @(y_true, y_pred) -mean(log10(y_pred), 'all');
+                self.loss_derivative = @(y_true, y_pred) 1./(log(10)*y_pred);
+            elseif name=="cross_entropy"
                 self.loss = @(y_true, y_pred) -sum(y_true*log10(y_pred), 'all');
                 self.loss_derivative = @(y_true, y_pred) -y_true/(y_pred*log(10));
             end
