f22f9e6c86590085c0d927f70f7e8428ec5d59e5
Omar Aflak <aflakomar@gmail.com>
2018-10-16 00:04:17 +0100

    used exp activation

--- examples/xor/main.m
+++ examples/xor/main.m
@@ -13,9 +13,9 @@ function [] = main()
     % create a 3-layer neural network
     net = Network();
     net.add_layer(FullyConnectedLayer([1 2], [1 3]));
-    net.add_layer(ActivationLayer([1 3], Activation("tanh")));
+    net.add_layer(ActivationLayer([1 3], Activation("exp")));
     net.add_layer(FullyConnectedLayer([1 3], [1 1]));
-    net.add_layer(ActivationLayer([1 1], Activation("tanh")));
+    net.add_layer(ActivationLayer([1 1], Activation("exp")));
     
     % set cost function and learning_rate
     net.build(Loss('mse'), 0.2)
