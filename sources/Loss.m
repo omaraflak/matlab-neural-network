2e5a35c70737bcea24219941137ce6677294133c
Omar Aflak <aflakomar@gmail.com>
2018-10-15 21:30:12 +0100

    fixed mse derivative

--- sources/Loss.m
+++ sources/Loss.m
@@ -8,7 +8,7 @@ classdef Loss < handle
         function self = Loss(name)
             if name=="mse"
                 self.loss = @(y_true, y_pred) mean((y_true - y_pred).^2, 'all');
-                self.loss_derivative = @(y_true, y_pred) 2*(y_pred - y_true);
+                self.loss_derivative = @(y_true, y_pred) (2/numel(y_true))*(y_pred - y_true);
             elseif name=="cross-entropy"
                 self.loss = @(y_true, y_pred) -sum(y_true*log10(y_pred), 'all');
                 self.loss_derivative = @(y_true, y_pred) -y_true/(y_pred*log(10));
