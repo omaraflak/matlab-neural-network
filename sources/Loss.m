277dc770ce68fbd88ed09d89eeccbb42998cbee7
Omar Aflak <aflakomar@gmail.com>
2018-10-15 22:20:39 +0100

    added Mean Squared Logarithmic Error (MSLE)

--- sources/Loss.m
+++ sources/Loss.m
@@ -9,6 +9,9 @@ classdef Loss < handle
             if name=="mse"
                 self.loss = @(y_true, y_pred) mean((y_true - y_pred).^2, 'all');
                 self.loss_derivative = @(y_true, y_pred) (2/numel(y_true))*(y_pred - y_true);
+            elseif name=="msle"
+                self.loss = @(y_true, y_pred) mean((log10(y_true+1)-log10(y_pred+1)).^2, 'all');
+                self.loss_derivative = @(y_true, y_pred) (-2/numel(y_true))*(log10(y_true+1)-log10(y_pred+1))/((log(10)*(y_pred+1)));
             elseif name=="cross-entropy"
                 self.loss = @(y_true, y_pred) -sum(y_true*log10(y_pred), 'all');
                 self.loss_derivative = @(y_true, y_pred) -y_true/(y_pred*log(10));
